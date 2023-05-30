c29c8dff830ceb2a0c8024398d6bc9122f3dccf3
ybalbino <yasmin.balbino@zup.com.br>
2023-05-30 00:54:19 -0300

    Initial commit

---
+++ src/Atividade3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args){
+
+        System.out.println("Calcule a media!");
+        System.out.println("-----------------------------------");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a sua primeira nota:");
+        double primeiraNota = entrada.nextDouble();
+
+        System.out.println("Digite a sua primeira nota:");
+        double segundaNota = entrada.nextDouble();
+
+        double primeiraMedia = (primeiraNota + segundaNota) / 2;
+
+        System.out.println("Primeira nota: " + primeiraNota);
+        System.out.println("Segunda nota: " + segundaNota);
+        System.out.println("Sua média parcial é: " + primeiraMedia);
+
+        System.out.println("-----------------------------------");
+
+        if(primeiraMedia >= 7.0){System.out.println("Situação até o momento: Aprovado");}
+        else if(primeiraMedia <= 6.0){System.out.println("Situação até o momento: Reprovado");}
+        else if(primeiraMedia == 10.0){System.out.println("Situação até o momento: Aprovado com DISTINCAO");}
+
+        System.out.println("-----------------------------------");
+    }
+}
